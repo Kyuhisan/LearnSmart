bb8cc64648e8c90be22d4448d27ed059330a06e3
pbevc03 <132909091+pbevc03@users.noreply.github.com>
2026-05-11 14:23:02 +0200

    Manjsi popravki, sprememba ai modela

--- Backend/src/main/java/com/learnSmart/learnSmart/Service/GeminiService.java
+++ Backend/src/main/java/com/learnSmart/learnSmart/Service/GeminiService.java
@@ -33,14 +33,14 @@ public class GeminiService {
             HttpHeaders headers = new HttpHeaders();
             headers.setContentType(MediaType.APPLICATION_JSON);
 
-            String url = "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=" + apiKey;
+            String url = "https://generativelanguage.googleapis.com/v1beta/models/gemini-3.1-flash-lite:generateContent?key=" + apiKey;
 
             ResponseEntity<Map> response = restTemplate.postForEntity(
                     url,
                     new HttpEntity<>(body, headers),
                     Map.class
             );
-
+            
             String result = extractText(response.getBody());
             return new LearningStyleResponse(result.trim(), 0.87); // <---- confidence zaenkrat hardcoded
 
@@ -54,7 +54,7 @@ public class GeminiService {
         try {
             var candidates = (List) body.get("candidates");
             var content = (Map) ((Map) candidates.get(0)).get("content");
-            var parts = (List) ((Map) content.get("parts"));
+            var parts = (List) (content.get("parts"));
             return (String) ((Map) parts.get(0)).get("text");
         } catch (Exception e) {
             return "UNCATEGORIZED";
