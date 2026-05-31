0c65cc4194b8bfe7dee3805cff4346ae43cf4ce0
Kyuhi <Kyuhisan@gmail.com>
2026-05-31 18:34:21 +0200

    SpotBugs fix

--- Backend/src/main/java/com/learnSmart/learnSmart/DTO/Quiz/ModulePerformanceDTO.java
+++ Backend/src/main/java/com/learnSmart/learnSmart/DTO/Quiz/ModulePerformanceDTO.java
@@ -6,4 +6,9 @@ import java.util.UUID;
 public record ModulePerformanceDTO(
         UUID predmetId, String naziv,
         int totalStudents, int avgScore, int avgCompletion, int passRate,
-        List<QuizPerformanceDTO> kvizi) {}
+        List<QuizPerformanceDTO> kvizi) {
+
+    public ModulePerformanceDTO {
+        kvizi = List.copyOf(kvizi);
+    }
+}
